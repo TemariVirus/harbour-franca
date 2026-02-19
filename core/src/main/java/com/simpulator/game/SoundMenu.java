ac620a1ecb9e3dfe869c655f2dd760cca64602a3
Fdlkq <2502287@sit.singaporetech.edu.sg>
2026-02-19 21:09:42 +0800

    Updated for volume slider
    TODO still on

--- core/src/main/java/com/simpulator/game/SoundMenu.java
+++ core/src/main/java/com/simpulator/game/SoundMenu.java
@@ -6,6 +6,12 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.simpulator.engine.Action;
 import com.simpulator.engine.ButtonManager.ButtonBindType;
 import com.simpulator.engine.GraphicsManager;
@@ -18,6 +24,8 @@ public class SoundMenu extends SwitchableScene {
     private MusicManager musics;
     private KeyboardManager km;
     private BitmapFont font;
+    private Stage stage;
+    private Slider volumeSlider;
 
     public SoundMenu(SceneManager sceneManager, MusicManager musicManager) {
         super(sceneManager);
@@ -37,6 +45,10 @@ public class SoundMenu extends SwitchableScene {
 
                 sounds.setVolume(target);
                 musics.setVolume(target);
+
+                if (volumeSlider != null) {
+                    volumeSlider.setValue(target); 
+                }
             }
         };
     }
@@ -61,13 +73,41 @@ public class SoundMenu extends SwitchableScene {
         km.bind(ButtonBindType.DOWN, Keys.LEFT, changeVolumeAction(-0.1f));
         km.bind(ButtonBindType.DOWN, Keys.RIGHT, changeVolumeAction(0.1f));
 
-        Gdx.input.setInputProcessor(km);
+        stage = new Stage();
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        volumeSlider = new Slider(0f, 1f, 0.1f, false, SimpleSkin.getSkin());
+        volumeSlider.setValue(sounds.getVolume());
+        
+        volumeSlider.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                float target = volumeSlider.getValue();
+                target = Math.round(target * 10) / 10.0f;
+                sounds.setVolume(target);
+                musics.setVolume(target);
+            }
+        });
+
+        table.add(volumeSlider).width(300).height(50).padTop(50);
+
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(stage); 
+        multiplexer.addProcessor(km);   
+        
+        Gdx.input.setInputProcessor(multiplexer);
     }
 
     @Override
     public void unload() {
         super.unload();
         font.dispose();
+        
+        if (stage != null) {
+            stage.dispose(); 
+        }
         Gdx.input.setInputProcessor(null);
     }
 
@@ -90,7 +130,7 @@ public class SoundMenu extends SwitchableScene {
             null
         );
         graphics.render(
-            new UIText(font, "[LEFT] / [RIGHT] to adjust", 100, 300),
+            new UIText(font, "[LEFT] / [RIGHT] or [MOUSE] to adjust", 100, 300),
             null
         );
 
@@ -99,5 +139,10 @@ public class SoundMenu extends SwitchableScene {
             null
         );
         graphics.endRender();
+
+        if (stage != null) {
+            stage.act(Gdx.graphics.getDeltaTime());
+            stage.draw();
+        }
     }
 }
