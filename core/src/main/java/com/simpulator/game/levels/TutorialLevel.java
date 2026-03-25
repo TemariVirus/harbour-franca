55c999983663a2e32058e5739463f62c5a7d0be0
Fdlkq <2502287@sit.singaporetech.edu.sg>
2026-03-25 19:25:21 +0800

    Level for tutorial

---
+++ core/src/main/java/com/simpulator/game/levels/TutorialLevel.java
@@ -0,0 +1,46 @@
+package com.simpulator.game.levels;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import com.badlogic.gdx.math.Vector3;
+import com.simpulator.game.entities.MerchantEntity;
+import com.simpulator.game.language.Language;
+import com.simpulator.game.levels.maps.MiramarMap;
+import com.simpulator.game.trading.Item;
+
+public class TutorialLevel extends Level {
+
+    public TutorialLevel() {
+        this.levelId = "level_00";
+        this.displayName = "Tutorial";
+        this.isTutorial = true;
+
+        this.nextLevelId = "level_01";
+
+        this.skyboxPath = "Skyboxes/miramar";
+        this.bgmPath = "GameAudio.mp3";
+        this.valueGoal = 7;
+        this.startingItems = new Item[] { Item.TRINKET };
+
+        this.playerStart = new Vector3(0, 1, 0);
+        
+        this.map = new MiramarMap(); 
+
+        this.merchants = new MerchantConfig[] {
+            new MerchantConfig(
+                MerchantEntity.class,
+                new Vector3(0, 0, 2),
+                new MerchantData(
+                    "Friendly Guide",
+                    "guide.png",
+                    Language.ENGLISH,
+                    "Welcome to Harbour Franca! Let's make a simple trade.",
+                    1.5f, 1.0f,
+                    new Item[] { Item.FISH, Item.CLOTH, Item.CANDLE },
+                    new HashSet<>(Arrays.asList(Item.TRINKET))
+                )
+            )
+        };
+    }
+}
